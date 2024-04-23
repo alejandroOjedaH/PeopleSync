985c63db06d8dd3727e4118979120833e31cc32e
alejandro <limonmagico1@gmail.com>
2024-04-23 22:22:09 +0200

    arreglo registro

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -15,7 +15,8 @@ export const User = sequelize.define(
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true,
     },
     email: {
       type: DataTypes.STRING,
